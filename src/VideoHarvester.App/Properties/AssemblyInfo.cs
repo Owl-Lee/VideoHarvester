853ea478569b67f1621857f1f68930ad546b21f9
Owl-Lee <liyanbao522@gmail.com>
2026-08-11 00:17:52 -0400

    Add application assembly metadata

---
+++ src/VideoHarvester.App/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("VideoHarvester.Core.Tests")]
+
+[assembly: AssemblyTitle("VideoHarvester")]
+[assembly: AssemblyDescription("本地视频保存工具")]
+[assembly: AssemblyCompany("VideoHarvester")]
+[assembly: AssemblyProduct("VideoHarvester PRO")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
